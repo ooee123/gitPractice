298dac9c8218500801960e0ebca4ed299f39276f
Kevin <ooee123@gmail.com>
2013-06-16 00:19:15 -0700

    first commit

---
+++ greetings.c
@@ -0,0 +1,15 @@
+/*
+ * This program prints the message "Hello World!" to the terminal
+ */
+#include <stdio.h>
+
+/*
+ * The starting point of the program
+ */
+int main()
+{
+   printf("Greetings from Kevin Ly!\n"); /* Standard Library function to 
+print stuff to terminal */
+
+   return 0; /* End of program, all is well, no errors, return 0 */
+}
